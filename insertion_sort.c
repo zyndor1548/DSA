c3ec4e3678ff3d022d1966d0adde09bd3ef6894d
zyndor1548 <a.s.aromal2006@gmail.com>
2025-10-14 20:14:35 +0530

    create insertion_sort.c

---
+++ insertion_sort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main() {
+	int max, a[50];
+	printf("Enter number of elements in the array : ");
+	scanf("%d", &max);
+	for (int i = 0; i < max; i++) {
+		printf("Enter %d element : ", i);
+		scanf("%d", &a[i]);
+	}
+
+	printf("Unsorted list : ");
+	for (int i = 0; i < max; i++) {
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+	int key;
+	for (int i = 0; i < max - 1; i++) {
+		key = a[i + 1];
+		if (a[i] > key) {
+			int j = i;
+			while (a[j] > key && j != -1) {
+				a[j + 1] = a[j];
+				j -= 1;
+			}
+			a[j + 1] = key;
+		}
+		for (int k = 0; k < max; k++) {
+			printf("%d ", a[k]);
+		}
+		printf("\n");
+	}
+	return 1;
+}

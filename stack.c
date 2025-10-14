bf03a5baf060cef222efe07945e992f473f831fe
zyndor1548 <a.s.aromal2006@gmail.com>
2025-10-14 20:14:57 +0530

    update stack.c

--- stack.c
+++ stack.c
@@ -1,75 +1,62 @@
 #include <stdio.h>
 
-int top = -1;
-int max;
-int stack[20];
+int top = -1, max, stack[50];
 
-int push();
-int pop();
-void display();
+void push() {
+	if (top >= max - 1) {
+		printf("stack overflow\n");
+		return;
+	}
+	printf("enter value to push : ");
+	top += 1;
+	scanf("%d", &stack[top]);
+}
+void pop() {
+	if (top == -1) {
+		printf("stack underflow\n");
+		return;
+	}
+	top -= 1;
+}
+void display() {
+	if (top == -1) {
+		printf("stack is empty\n");
+	}
+	printf("stack --> \n");
+	for (int i = top; i >= 0; i--) {
+		printf("%d \n", stack[i]);
+	}
+	printf("\n");
+}
 
 int main() {
-    printf("Enter number of elements in the stack : ");
-    scanf("%d", &max);
-
-    int menu;
-    do {
-        printf("\nStack Functions");
-        printf("\n1. Display");
-        printf("\n2. Push");
-        printf("\n3. Pop");
-        printf("\n4. Exit");
-        printf("\nChoose: ");
-        scanf("%d", &menu);
-
-        switch (menu) {
-            case 1:
-                display();
-                break;
-            case 2:
-                push();
-                break;
-            case 3:
-                pop();
-                break;
-            case 4:
-                printf("Exiting...\n");
-                break;
-            default:
-                printf("Invalid choice. Try again.\n");
-        }
-
-    } while (menu != 4);
-
-    return 0;
-}
-
-int push() {
-    if (top >= max - 1) {
-        printf("Stack Overflow\n");
-        return 0;
-    }
-    printf("Enter value to push: ");
-    scanf("%d", &stack[++top]);
-    printf("Pushed %d to the stack.\n", stack[++top]);
-    return 1;
-}
-
-int pop() {
-    if (top == -1) {
-        printf("Stack Underflow\n");
-        return 0;
-    }
-    printf("You popped: %d\n", stack[top--]);
-    return 1;
-}
-
-void display() {
-
-    printf("Current stack:\n");
-    for (int i = max; i >= 0; i--) {
-        printf("-----\n");
-        printf("| %d |\n", stack[i]);
-    }
-    printf("-----\n");
+	printf("Enter total number of elements in the stack : ");
+	scanf("%d", &max);
+	int menu;
+	do {
+		printf("\tstack menu\n");
+		printf("\t1. display\n");
+		printf("\t2. push\n");
+		printf("\t3. pop\n");
+		printf("\t4. exit\n");
+		printf("\t enter choice : ");
+		scanf("%d", &menu);
+		switch (menu) {
+		case 1:
+			display();
+			break;
+		case 2:
+			push();
+			break;
+		case 3:
+			pop();
+			break;
+		case 4:
+			printf("exiting...");
+			break;
+		default:
+			printf("Invalid input");
+		}
+	} while (menu != 4);
+	return 0;
 }
